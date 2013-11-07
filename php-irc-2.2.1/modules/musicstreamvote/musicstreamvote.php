4dd3b8f3186958aa2f10dd2c6b64b82ed2d00eca
Brendan Kidwell <snarf@glump.net>
2013-11-07 01:53:36 -0500

    added PHP DocBlocks for bot

--- php-irc-2.2.1/modules/musicstreamvote/musicstreamvote.php
+++ php-irc-2.2.1/modules/musicstreamvote/musicstreamvote.php
@@ -1,22 +1,84 @@
 <?php
 define( 'MUSICSTREAMVOTE_DEBUG', TRUE );
 
+/**
+ * musicstreamvote module for PHP-IRC bot
+ *
+ * @author  Brendan Kidwell <snarf@glump.net>
+ * @license  GPL3
+ * @package  music-stream-vote
+ */
 class musicstreamvote extends module {
 
+    /**
+     * Module title
+     * @var string
+     */
     public $title = 'Music Stream Vote';
+    /**
+     * Module author
+     * @var string
+     */
     public $author = 'Brendan Kidwell';
+    /**
+     * Module version
+     * @var string
+     */
     public $version = '1.0';
 
+    /**
+     * Absolute filesystem path to module, with trailing slash
+     * @var string
+     */
     private $mod_dir = '';
+    /**
+     * Key-Value list of application Options from WordPress
+     * @var string[]
+     */
     private $options = array();
+    /**
+     * List of channels bot is currently joined to
+     * @var string[]
+     */
     private $in_channels = array();
+    /**
+     * Key-Value list of active timers
+     * @var mixed[]
+     */
     private $timers = array();
+    /**
+     * Key-Value list of active CURL objects (for querying IceCast, WordPress)
+     * @var mixed[]
+     */
     private $curl = array();
+    /**
+     * Last 'stream_title'
+     * @var string
+     */
     private $now_playing = '';
+    /**
+     * Last IRC announcement for 'stream_title'
+     * @var string
+     */
     private $now_playing_response = '';
+    /**
+     * Votes that are waiting for WHOIS responses from IRC server
+     * @var mixed[]
+     */
     private $pending_votes = array();
+    /**
+     * Remember last error message so it isn't repeated.
+     * @var string
+     */
     private $last_error = '';
 
+    /**
+     * Start bot.
+     *
+     * Invoked by the framework.
+     * 
+     * @return [type]
+     */
     public function init() {
         $this->dbg( 'init()' );
 
@@ -36,6 +98,10 @@ class musicstreamvote extends module {
         $this->curl['streaminfo'] = FALSE;
     }
 
+    /**
+     * Shut down bot.
+     * @return [type]
+     */
     public function destroy() {
         $this>dbg( 'destroy()' );
         foreach ( $this->timers as $key => $value ) {
@@ -46,6 +112,13 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Bot is logged into a channel and ready to start doing work.
+     *
+     * Invoked by the framework.
+     * 
+     * @return void
+     */
     private function evt_logged_in() {
         if ( count($this->in_channels) > 1 ) { return; }
 
@@ -58,6 +131,13 @@ class musicstreamvote extends module {
         $this->evt_stream_poll();
     }
 
+    /**
+     * Poll IceCast server to get current stream_title.
+     *
+     * Invoked by the framework.
+     * 
+     * @return boolean
+     */
     public function evt_stream_poll() {
         $this->dbg( 'entering evt_stream_poll()' );
         $data = $this->streaminfo( $this->options['stream_status_url'] );
@@ -84,6 +164,13 @@ class musicstreamvote extends module {
         return TRUE;
     }
 
+    /**
+     * Poll for existence of './restart' file to see if we need to abort and reload IRC bot.
+     *
+     * Invoked by the framework.
+     * 
+     * @return boolean
+     */
     public function evt_restart_poll() {
         if ( file_exists( $this->mod_dir . 'restart' ) ) {
             $this->announce ( 'is being restarted.', TRUE );
@@ -93,10 +180,25 @@ class musicstreamvote extends module {
         return TRUE;
     }
 
+    /**
+     * Abort IRC bot in order to restart.
+     * @return void
+     */
     public function evt_abort() {
         die();
     }
 
+    /**
+     * Process raw private/channel/notice messages.
+     *
+     * Invoked by the framework.
+     *
+     * Look for: 1) WHOIS responses for votes; 2) Newbie who hasn't found the Help command.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function evt_raw( $line, $args ) {
         $cmd = $line['cmd'];
         if ( $cmd == 307 || $cmd == 318 ) {
@@ -117,6 +219,12 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Process WHOIS reply.
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     private function evt_whois( $line, $args ) {
         $cmd = $line['cmd'];
         $subject = $line['params'];
@@ -129,6 +237,12 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Process newbie query and suggest saying '!$help_cmd'.
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     private function evt_sayhi( $line, $args ) {
         print_r($line);
         $response = $this->webservice( 'sayhi', array(
@@ -139,6 +253,15 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Display Help from WordPress.
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function cmd_help( $line, $args ) {
         $response = $this->webservice( 'help', array(), $line );
         if ( $response['output'] ) {
@@ -146,12 +269,30 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Display last Now Playing announcement.
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function cmd_nowplaying( $line, $args ) {
         if ( $this->now_playing_response ) {
             $this->reply( $line, $this->now_playing_response );
         }
     }
 
+    /**
+     * Submit vote to WordPress. (Part 1.)
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function cmd_vote( $line, $args ) {
         $fromNick = $line['fromNick'];
         $vote = array(
@@ -167,6 +308,14 @@ class musicstreamvote extends module {
         $this->ircClass->sendRaw( "WHOIS $fromNick" );
     }
 
+    /**
+     * Submit vote to WordPress. (Part 2.)
+     *
+     * Invoked by the evt_whois().
+     * 
+     * @param  string $subject nick who owns the pending vote
+     * @return void
+     */
     public function cmd_vote_finish( $subject ) {
         $vote = $this->pending_votes[$subject];
         $line = $vote['line'];
@@ -185,6 +334,15 @@ class musicstreamvote extends module {
         unset($this->pending_votes[$subject]);
     }
 
+    /**
+     * Undo last vote by this nick.
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function cmd_unvote( $line, $args ) {
         $response = $this->webservice( 'undo_vote', array(
             'nick' => $line['fromNick'],
@@ -195,16 +353,43 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Vote +3.
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function cmd_like( $line, $args ) {
         $args['query'] = '+3';
         $this->cmd_vote( $line, $args );
     }
 
+    /**
+     * Vote -3.
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function cmd_hate( $line, $args ) {
         $args['query'] = '-3';
         $this->cmd_vote( $line, $args );
     }
 
+    /**
+     * Get top ten tracks by vote sum from WordPress.
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @param  string[] $args
+     * @return void
+     */
     public function cmd_stats( $line, $args ) {
         $response = $this->webservice( 'stats', array(), $line );
         if ( $response['output'] ) {
@@ -212,6 +397,14 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * The bot has joined a channel.
+     *
+     * Invoked by the framework.
+     * 
+     * @param  string[] $line
+     * @return void
+     */
     public function evt_join( $line ) {
         if ( $this->options['irc_nick'] == $line['fromNick'] ) {
             $this->dbg( 'evt_join(): ' . $line['text'] );
@@ -220,6 +413,12 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Reply to nick (PM) or channel (not PM)
+     * @param  string[] $line the data received from the framework for this event
+     * @param  string $text what to say
+     * @return void
+     */
     private function reply( $line, $text ) {
         if ( $line['to'] == $this->options['irc_nick'] ) {
             $to = $line['fromNick'];
@@ -236,6 +435,12 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Announce to all channels bot is currently in.
+     * @param  string  $text
+     * @param  boolean $action Use "/me" form?
+     * @return void
+     */
     private function announce( $text, $action = FALSE ) {
         $text = str_ireplace(
             array('<b>', '</b>'),
@@ -257,10 +462,22 @@ class musicstreamvote extends module {
         }
     }
 
+    /**
+     * Write to debug log.
+     * @param  string $text
+     * @return void
+     */
     private function dbg( $text ) {
         $this->ircClass->log( "[MSV] $text" );
     }
 
+    /**
+     * Invoke a method in the WordPress web service.
+     * @param  string $method
+     * @param  mixed[] $args Key-Value list of arguments
+     * @param  string[] $line the data received from the framework for this event
+     * @return mixed[]
+     */
     private function webservice( $method, $args, $line = NULL ) {
         $args['web_service_password'] = $this->options['web_service_password'];
 
@@ -310,6 +527,11 @@ class musicstreamvote extends module {
         return $data;
     }
 
+    /**
+     * Get current 'stream_title' from IceCast.
+     * @param  string $url
+     * @return string
+     */
     private function streaminfo( $url ) {
         if ( $this->curl['streaminfo'] === FALSE ) {
             $this->curl['streaminfo'] = curl_init();
@@ -352,6 +574,11 @@ class musicstreamvote extends module {
         return $data;
     }
 
+    /**
+     * Create a repeating timer event.
+     * @param string $function_name callback function name in this module
+     * @param int $interval_sec
+     */
     private function add_timer( $function_name, $interval_sec ) {
         $timer_name = 'msv_' . $function_name;
         $this->dbg( "add_timer($timer_name, $function_name, $interval_sec)" );
