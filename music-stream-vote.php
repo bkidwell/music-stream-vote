b202f5f4be7fd61833bca0a7a7007435c695f3bd
Brendan Kidwell <snarf@glump.net>
2013-11-07 01:36:26 -0500

    cleanup plugin loader

--- music-stream-vote.php
+++ music-stream-vote.php
@@ -11,21 +11,62 @@ License: GPL 3
 
 namespace GlumpNet\WordPress\MusicStreamVote;
 
+/**
+ * Must be running at least this PHP version.
+ */
 define( __NAMESPACE__ . '\\REQUIRE_PHP_VER', '5.4.0' );
+/**
+ * Absolute filesystem path to Music Stream Vote plugin, with trailing slash
+ */
 define( __NAMESPACE__ . '\\PLUGIN_DIR', dirname( __FILE__ ) . '/' );
+/**
+ * Absolute URL to Music Stream Vote plugin files, with trailing slash
+ */
 define( __NAMESPACE__ . '\\PLUGIN_URL', plugins_url( basename( dirname( __FILE__ ) ) ) . '/' );
+/**
+ * Application name
+ */
 define( __NAMESPACE__ . '\\PLUGIN_NAME', 'Music Stream Vote' );
+/**
+ * Application code name
+ */
 define( __NAMESPACE__ . '\\PLUGIN_SLUG', 'musicstreamvote' );
+/**
+ * Database table prefix
+ */
 define( __NAMESPACE__ . '\\PLUGIN_TABLESLUG', 'musvote' );
+/**
+ * Absolute filesystem path to IRC bot, with trailing slash
+ */
 define( __NAMESPACE__ . '\\BOT_DIR', PLUGIN_DIR . 'php-irc-2.2.1/' );
 
+/**
+ * Max length in DB for stream_title
+ */
 define( __NAMESPACE__ . '\\DB_STREAM_TITLE_LEN', 200 );
+/**
+ * Max length in DB for nick
+ */
 define( __NAMESPACE__ . '\\DB_NICK_LEN', 30 );
+/**
+ * Max length in DB for user_id
+ */
 define( __NAMESPACE__ . '\\DB_USER_ID_LEN', 150 );
+/**
+ * Max length in DB for track artist
+ */
 define( __NAMESPACE__ . '\\DB_ARTIST_LEN', 100 );
+/**
+ * Max length in DB for track title
+ */
 define( __NAMESPACE__ . '\\DB_TITLE_LEN', 100 );
 
-spl_autoload_register(__NAMESPACE__ . '\\autoload');
+spl_autoload_register( __NAMESPACE__ . '\\autoload' );
+/**
+ * Class loader for the plugin (called by PHP)
+ * @param  string $cls Class name
+ * @return void
+ */
 function autoload( $cls ) {
     $c = ltrim( $cls, '\\' ); $l = strlen( __NAMESPACE__ );
     if ( strncmp( $c, __NAMESPACE__, $l ) !== 0 ) { return; }
@@ -36,6 +77,10 @@ function autoload( $cls ) {
     require_once( $f );
 }
 
+/**
+ * Wrong version of PHP found.
+ * @return void
+ */
 function php_fail() {
     ?>
     <div class="error">
@@ -54,15 +99,13 @@ if (version_compare(phpversion(), REQUIRE_PHP_VER, ">=")) {
 
     register_activation_hook( __FILE__, 'musicstreamvote_install' );
     function musicstreamvote_install() {
-        VotePlugin::Installed();
     }
     register_deactivation_hook( __FILE__, 'musicstreamvote_remove' );
     function musicstreamvote_remove() {
-        VotePlugin::Removed();
     }
 
 } else {
 
-    add_action( 'admin_notices', 'GlumpNet\\WordPress\\MusicStreamVote\\php_fail' );
+    add_action( 'admin_notices', __NAMESPACE__ . '\\php_fail' );
 
 } 
