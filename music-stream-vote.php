8d4d4c572743fa0951085da5d2fb7c66b3907a22
Brendan Kidwell <snarf@glump.net>
2014-01-20 01:07:28 -0500

    move defines to a separate file

--- music-stream-vote.php
+++ music-stream-vote.php
@@ -11,63 +11,7 @@ License: GPL 3
 
 namespace GlumpNet\WordPress\MusicStreamVote;
 
-/**
- * Must be running at least this PHP version.
- */
-define( __NAMESPACE__ . '\\REQUIRE_PHP_VER', '5.4.0' );
-/**
- * Absolute filesystem path to Music Stream Vote plugin, with trailing slash
- */
-define( __NAMESPACE__ . '\\PLUGIN_DIR', dirname( __FILE__ ) . '/' );
-/**
- * Absolute URL to Music Stream Vote plugin files, with trailing slash
- */
-define( __NAMESPACE__ . '\\PLUGIN_URL', plugins_url( basename( dirname( __FILE__ ) ) ) . '/' );
-/**
- * Application name
- */
-define( __NAMESPACE__ . '\\PLUGIN_NAME', 'Music Stream Vote' );
-/**
- * Application code name
- */
-define( __NAMESPACE__ . '\\PLUGIN_SLUG', 'musicstreamvote' );
-/**
- * Database table prefix
- */
-define( __NAMESPACE__ . '\\PLUGIN_TABLESLUG', 'musvote' );
-/**
- * Absolute filesystem path to IRC bot, with trailing slash
- */
-define( __NAMESPACE__ . '\\BOT_DIR', PLUGIN_DIR . 'php-irc-2.2.1/' );
-
-/**
- * Max length in DB for stream_title
- */
-define( __NAMESPACE__ . '\\DB_STREAM_TITLE_LEN', 200 );
-/**
- * Max length in DB for nick
- */
-define( __NAMESPACE__ . '\\DB_NICK_LEN', 30 );
-/**
- * Max length in DB for user_id
- */
-define( __NAMESPACE__ . '\\DB_USER_ID_LEN', 150 );
-/**
- * Max length in DB for track artist
- */
-define( __NAMESPACE__ . '\\DB_ARTIST_LEN', 100 );
-/**
- * Max length in DB for track title
- */
-define( __NAMESPACE__ . '\\DB_TITLE_LEN', 100 );
-/**
- * Max length in DB for comment
- */
-define( __NAMESPACE__ . '\\DB_COMMENT_LEN', 200 );
-/**
- * Database schema version -- increment this every time you change Db::update_schema() .
- */
-define( __NAMESPACE__ . '\\DB_VERSION', 3 );
+require_once( dirname( __FILE__ ) . '/defines.php' );
 
 spl_autoload_register( __NAMESPACE__ . '\\autoload' );
 /**
